5eedf0acba8679ab8f81ff12249f9d08e5007a52
Omar Besbes <103886190+ombesbes@users.noreply.github.com>
2025-09-19 20:52:47 +0100

    Update CI CD settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,12 +28,13 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost', 
-'https://ombesbes-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 
-'https://ombesbes-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai' ]
+ALLOWED_HOSTS=['localhost',
+                'https://ombesbes-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+                'https://ombesbes-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
-CSRF_TRUSTED_ORIGINS=['https://ombesbes-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
- 'https://ombesbes-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS=[
+                      'https://ombesbes-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+                       'https://ombesbes-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(BASE_DIR,'frontend/static'),
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/static'),
                   os.path.join(BASE_DIR, 'frontend/build'),
                   os.path.join(BASE_DIR, 'frontend/build/static'), ],
         'APP_DIRS': True,
@@ -96,7 +97,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.\
+        UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -140,7 +142,6 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [ os.path.join(BASE_DIR,'frontend/static'),
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/static'),
                      os.path.join(BASE_DIR, 'frontend/build'),
-                     os.path.join(BASE_DIR, 'frontend/build/static') ]
-
+                     os.path.join(BASE_DIR, 'frontend/build/static')]
