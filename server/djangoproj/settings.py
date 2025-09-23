90a8283032ee8e5e09a7813acafc0eddb30d6a91
Omar Besbes <ombesbes@gmail.com>
2025-09-23 11:50:09 -0400

    fixed linting errors

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -40,8 +40,7 @@ ALLOWED_HOSTS = ['localhost',
 CSRF_TRUSTED_ORIGINS = [
                         'https://ombesbes-8000.\
                         theianext-1-labs-prod-misc-tools-us-east-0\
-                        .proxy.cognitiveclass.ai'
-                        ,
+                        .proxy.cognitiveclass.ai',
                         'https://ombesbes-8000.\
                         theiadockernext-1-labs-prod-theiak8s-4-tor01\
                         .proxy.cognitiveclass.ai'
