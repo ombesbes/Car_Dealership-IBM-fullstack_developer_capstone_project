1f64e4b7fb248d5abbbfc0a90293909d3dc11b02
Omar Besbes <ombesbes@gmail.com>
2025-09-23 10:34:28 -0400

    fixing linting errors

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,17 +30,21 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
                  'https://ombesbes-8000.\
-                 theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+                 theianext-1-labs-prod-misc-tools-us-east-0.\
+                 proxy.cognitiveclass.ai',
                  'https://ombesbes-8000.\
-                 theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+                 theiadockernext-1-labs-prod-theiak8s-4-tor01.\
+                 proxy.cognitiveclass.ai'
                  ]
 
 CSRF_TRUSTED_ORIGINS = [
                         'https://ombesbes-8000.\
-                        theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
-                         ,
+                        theianext-1-labs-prod-misc-tools-us-east-0\
+                        .proxy.cognitiveclass.ai'
+                        ,
                         'https://ombesbes-8000.\
-                        theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+                        theiadockernext-1-labs-prod-theiak8s-4-tor01\
+                        .proxy.cognitiveclass.ai'
                         ]
 
 REST_FRAMEWORK = {
